a5224600ec7781a3feca3b818a9757c1270733aa
Ben Jarlett <emailme@benjarlett.co.uk>
2013-05-30 17:06:13 +0100

    Bugfix on what where how why menu

--- js/wwhw_menu.js
+++ js/wwhw_menu.js
@@ -1,6 +1,16 @@
 Drupal.behaviors.modulename_subidentifier = function(context) { 
 	//code goes here
-	$( "#intronav" ).accordion({
-	collapsible: true
+	$('#intronav > li > a').click(function(e){
+		var a = $(this);
+		var li = a.parent();
+		li.parent().children('li').children('ul').hide();
+	
+		li.children('ul').slideDown(300);
+		if(li.children('ul').length() > 0) {
+	     	return false;
+		}
 	});
+	
+	
 }
+
