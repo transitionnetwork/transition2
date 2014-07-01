540be27ee5551ccc64fcde6f91090e7e6036bca8
Ben Jarlett <emailme@benjarlett.co.uk>
2014-07-01 19:16:45 +0100

    Compressing css
    now that we’ve found out the problem with the syncing.. ben was looking
    at:
    https://stg.transitionnetwork.org/blogs/rob-hopkins
    but the changes appeared on:
    https://booker-stage-20140501.transitionnetwork.org/blogs/rob-hopkins
    
    now works as expected.

--- config.rb
+++ config.rb
@@ -29,4 +29,5 @@ preferred_syntax = :scss
 line_comments = false # by Compass.app 
 sass_options = {:debug_info=>false} # by Compass.app 
 
-output_style = :expanded # by Compass.app 
+
+output_style = :compressed # by Compass.app 
